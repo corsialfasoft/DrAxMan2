7c1683d10558ce4cf1b6fa4810d6200ccc5a13d5
TandyD <brinzila30@gmail.com>
2018-04-10 16:36:34 +0200

    DettagliProdotto

--- UC0012/Controllers/HomeController.cs
+++ UC0012/Controllers/HomeController.cs
@@ -2,27 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using UC0012.Models;
 using System.Web.Mvc;
 
 namespace UC0012.Controllers
 {
 	public class HomeController : Controller
 	{
+        IDomainModel db;
 		public ActionResult Index()
 		{
 			return View();
 		}
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
+		public ActionResult DettaglioProdotto(){
 			return View();
 		}
 
+        public ActionResult Define(String id)
+		{   
+            db = new DomainModel();
+            Prodotto prod = db.SearchProdotto(int.Parse(id));
+            if(prod != null){ 
+                ViewBag.prodotto = prod;
+            }else{ 
+                ViewBag.Message = "Prodotto inesistente";    
+            }
+			return View("DettaglioProdotto");
+		}
         
-		public ActionResult ListaProdotti()
-		{
+		public ActionResult ListaProdotti(){
 			return View();
 		}
 	}
